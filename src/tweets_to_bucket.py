14f467ee6f758f01018eb0631df2c0535b8a7e59
Brent <blemieux4@gmail.com>
2017-03-20 08:52:02 -0600

    aws mods

--- src/tweets_to_bucket.py
+++ src/tweets_to_bucket.py
@@ -12,26 +12,22 @@ import os
 
 import json
 
-def to_bucket():
-    with open('../../amazon.json') as key_file:
-        keys = json.load(key_file)
-        access_key = keys["AWS_ACCESS_KEY_ID"]
-        access_secret_key = keys["AWS_SECRET_ACCESS_KEY"]
+with open('../../amazon.json') as key_file:
+    keys = json.load(key_file)
+    access_key = keys["AWS_ACCESS_KEY_ID"]
+    access_secret_key = keys["AWS_SECRET_ACCESS_KEY"]
 
-    file_name = '../../tweets/{}'.format(os.listdir('../../tweets')[0])
-    bucket_name = "tasty-tweets"
-    fil = open(file_name)
-    conn = boto.connect_s3(access_key,access_secret_key)
-    bucket = conn.get_bucket(bucket_name)
-    #Get the Key object of the bucket
-    k = Key(bucket)
-    #Crete a new key with id as the name of the file
-    k.key = file_name
-    #Upload the file
-    result = k.set_contents_from_file(fil)
-    #result contains the size of the file uploaded
+file_name = '../../tweets/{}'.format(os.listdir('../../tweets')[0])
+bucket_name = "tasty-tweets"
+fil = open(file_name)
+conn = boto.connect_s3(access_key,access_secret_key)
+bucket = conn.get_bucket(bucket_name)
+#Get the Key object of the bucket
+k = Key(bucket)
+#Crete a new key with id as the name of the file
+k.key = file_name
+#Upload the file
+result = k.set_contents_from_file(fil)
+#result contains the size of the file uploaded
 
-    os.remove(file_name)
-
-if __name__ == '__main__':
-    to_bucket()
+os.remove(file_name)
