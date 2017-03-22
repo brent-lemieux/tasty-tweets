8351beb09626cbb52beefbfa64f9ed4dee2f36ed
Brent <blemieux4@gmail.com>
2017-03-22 14:03:25 -0600

    aws mods

--- src/pipe_to_bucket.py
+++ src/pipe_to_bucket.py
@@ -7,6 +7,6 @@ import time
 
 if __name__ == '__main__':
     date = time.strftime("%m-%d-%Y--%H-%M")
-    topics = ['chipotle']
-    get_tweets(topics, '/home/ubuntu/tweets/tweets-{}'.format(date), 1)
+    topics = ['climate']
+    get_tweets(topics, '/home/ubuntu/tweets/tweets-{}'.format(date), 100)
     to_bucket()
